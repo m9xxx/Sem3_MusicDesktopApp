c398a980985ed5e3567088ce427a9298d2c5277e
max <k2002mv@gmail.com>
2022-12-18 11:51:54 +0300

    Added IDs for elements on scene

--- src/main/java/com/example/sem3_project/HelloController.java
+++ src/main/java/com/example/sem3_project/HelloController.java
@@ -1,14 +1,52 @@
 package com.example.sem3_project;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.*;
+import javafx.scene.layout.VBox;
 
 public class HelloController {
     @FXML
     private Label welcomeText;
-
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
-    }
+    private TextField SearchField;
+    @FXML
+    private Button SearchButton;
+    @FXML
+    private VBox OBJ_00;
+    @FXML
+    private VBox OBJ_01;
+    @FXML
+    private VBox OBJ_02;
+    @FXML
+    private VBox OBJ_10;
+    @FXML
+    private VBox OBJ_11;
+    @FXML
+    private VBox OBJ_12;
+    @FXML
+    private VBox OBJ_20;
+    @FXML
+    private VBox OBJ_21;
+    @FXML
+    private VBox OBJ_22;
+    @FXML
+    private Button Track_Previous;
+    @FXML
+    private Button Track_StartStop;
+    @FXML
+    private Button Track_Forward;
+    @FXML
+    private TextField Current_Artist;
+    @FXML
+    private TextField Current_Track;
+    @FXML
+    private TextField Current_Cover;
+    @FXML
+    private Slider Volume_Slider;
+    @FXML
+    private Button RepeatButton;
+    @FXML
+    private ProgressBar TrackProgress;
+
+
 }
