b93b30cf70168c77ba8105d3fdbd58b3a4f568b4
alexandor <gasan.sacha@mail.ru>
2022-12-25 16:46:46 +0700

    Commit

--- src/main/java/com/example/sem3_project/HelloApplication.java
+++ src/main/java/com/example/sem3_project/HelloApplication.java
@@ -12,7 +12,7 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1200, 800);
-        stage.setTitle("JavaMedia");
+        stage.setTitle("Java Media");
         stage.setScene(scene);
         stage.setResizable(false);
         stage.show();
